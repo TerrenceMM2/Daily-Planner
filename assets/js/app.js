801fcbf6acf2e4fb5a976496dec7bba29058f67a
TerrenceMM2 <terrencemm2@gmail.com>
2020-02-07 14:49:20 -0600

    changes cell color based on time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     const timezone = moment.tz.guess();
+    const currentTime = moment().hour();
+
+    console.log(currentTime);
 
     $("#current-date").text(moment().format('dddd[,] MMMM Do[,] YYYY'));
     $("#current-time").text(moment().tz(timezone).format('h[:]mm A z'));
 
-    $("#container").on("click", "i", function(event){ 
-        let id = event.target.id;
-        saveBlock(id);
-    });
+    for (var i = 0; i < 24; i++) {
+        let hourDay = "AM";
+        let hour = i;
+        let bgColor;
 
-    const hourBlock = (num) => {
-        for (var i = 0; i < num; i++) {
-
-            let hourDay = "AM";
-            let hour = i;
-
-            if (i > 3) {
+        if (i > 8 && i < 18) {
+            console.log(i)
+            if (i > 12) {
                 hourDay = "PM";
                 hour = i - 12;
             }
 
+            if (currentTime > i) {
+                bgColor = "past";
+            } else if (currentTime == i) {
+                bgColor = "current";
+            } else {
+                bgColor = "future";
+            }
+
             const container = $("#container");
             const row = $("<div>").addClass("row")
-            const hourCell = $("<div>").addClass("hour-cell col-1").text((hour + 9) + " " + hourDay);
+            const hourCell = $("<div>").addClass("hour-cell col-1").text(hour + " " + hourDay);
             const textCell = $("<textarea>").addClass("text-cell col-10").attr("id", "block-" + i);
+            textCell.addClass(bgColor);
             const saveCell = $("<div>").addClass("save-cell col-1 m-auto")
             const icon = $("<i>").addClass("fas fa-save fa-lg").attr("id", i);
             saveCell.append(icon);
@@ -32,13 +40,5 @@ $(document).ready(function() {
             container.append(row);
         }
     }
-
-    const saveBlock = (num) => {
-        let blockRef = "#block-" + num;
-        let block = $(blockRef).text();
-        console.log(block);
-    }
-
-    hourBlock(9);
 
 });
